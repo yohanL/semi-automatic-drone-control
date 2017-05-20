e4fe636e258a1d2cac023265f5540a99c050c7ef
yohanL <m.lee.yohan@gmail.com>
2017-05-20 17:47:58 +0200

    Add files via upload

--- intersection.py
+++ intersection.py
@@ -5,28 +5,38 @@ import numpy as np
 import os
 
 def getIntersection(line1, line2):
-    s1 = np.array([line1[[0],[0]],line1[[0],[1]]])
-    e1 = np.array([line1[[0],[2]],line1[[0],[3]]])
 
-    s2 = np.array([line2[[0],[0]],line2[[0],[1]]])
-    e2 = np.array([line2[[0],[2]],line2[[0],[3]]])
-
-    a1 = (s1[1] - e1[1]) / (s1[0] - e1[0])
-    b1 = s1[1] - (a1 * s1[0])
-
-    a2 = (s2[1] - e2[1]) / (s2[0] - e2[0])
-    b2 = s2[1] - (a2 * s2[0])
-
-    #if abs(a1 - a2) < sys.float_info.epsilon:
-        #return False
-
-    x = (b2 - b1) / (a1 - a2)
-    y = a1 * x + b1
-
-    x1 = float(x)
-    y1 = float(y)
+    x1=line1[0]
+    y1=line1[1]
+    x2=line1[2]
+    y2=line1[3]
+    x3=line2[0]
+    y3=line2[1]
+    x4=line2[2]
+    y4=line2[3]
+    a1=(y2-y1)/(x2-x1)
+    b1=y1-a1*x1
+    a2=(y4-y3)/(x4-x3)
+    b2=y3-a2*x3
+    x=(b1-b2)/(a2-a1)
+    y=a1*x+b1
     
-    return np.array([x1,y1])
+    return np.array([x,y])
 
+def getIntersectionV(line1, line2):
 
+    x1=line1[0]
+    y1=line1[1]
+    x2=line1[2]
+    y2=line1[3]
+    x3=line2[0]
+    y3=line2[1]
+    x4=line2[2]
+    y4=line2[3]
+    
+    a=(y4-y3)/(x4-x3)
+    b=y3-a*x3
+    y=a*x1+b
+    
+    return np.array([x1,y])
     
